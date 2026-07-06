fad7c910d86d84d3e12eacaea30a93e2cbb38740
zebraonkz <zebraonkz@gmail.com>
2026-07-06 20:00:18 +0500

    Update metro.config.js to customize module resolution for react-native-worklets and add new dependencies in package.json.

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,35 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+const defaultResolveRequest = config.resolver.resolveRequest;
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === 'react-native-worklets') {
+    return {
+      filePath: path.resolve(
+        __dirname,
+        'node_modules/react-native-worklets/lib/module/index.js',
+      ),
+      type: 'sourceFile',
+    };
+  }
+
+  if (moduleName === 'react-native-worklets/plugin') {
+    return {
+      filePath: path.resolve(
+        __dirname,
+        'node_modules/react-native-worklets/plugin/index.js',
+      ),
+      type: 'sourceFile',
+    };
+  }
+
+  if (defaultResolveRequest) {
+    return defaultResolveRequest(context, moduleName, platform);
+  }
+
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+module.exports = config;
